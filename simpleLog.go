86fc3d839d9f5ce88a1b6a7a9b171e887bdb41ee
xhq <xuh16807@gmail.com>
2025-07-24 21:01:25 +0800

    test end 2

--- simpleLog.go
+++ simpleLog.go
@@ -1,7 +1,32 @@
 package simpleLog
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
-func Test() {
-	fmt.Println("simpleLog")
+var logFile *os.File
+
+func init() {
+	err := os.MkdirAll("./log", os.ModePerm)
+	if err != nil {
+		log.Println("初始化失败：无法创建 log 目录 -", err)
+		return
+	}
+
+	filePath := "./log/log.txt"
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println("初始化失败：无法打开 log.txt 文件 -", err)
+		return
+	}
+	logFile = f
+	// 不要关闭logFile，保持打开状态，方便写日志
+}
+
+// WriteLog 其他地方写日志
+func WriteLog(msg string) {
+	if logFile != nil {
+		logFile.WriteString(msg + "\n")
+	}
 }
